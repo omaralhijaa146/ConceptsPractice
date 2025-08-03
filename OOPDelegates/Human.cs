1213e26a15c03cef9a2fea9f86f13e9efec0ef76
omar abu alhijaa <omaribraheem222@hotmail.com>
2025-08-03 14:30:48 +0300

    Practicing delegates

---
+++ OOPDelegates/Human.cs
@@ -0,0 +1,33 @@
+﻿namespace OOPDelegates;
+
+public class Human
+{
+    protected string Name { get; set; }
+    protected int Age { get; set; }
+    
+    protected Func<string>  HumanThinking;
+
+    public Human(string name, int age, Func<string> humanThinking)
+    {
+        Name = name;
+        Age = age;
+        HumanThinking = humanThinking;
+    }
+
+    public void DisplayHumanProperties()
+    {
+        Console.WriteLine();
+        Console.WriteLine("****************************");
+        Console.WriteLine("****************************");
+        Console.WriteLine();
+        Console.WriteLine("Human Properties");
+        Console.WriteLine();
+        Console.WriteLine("****************************");
+        Console.WriteLine();
+        Console.WriteLine($"Human name => {Name}");
+        Console.WriteLine($"Human age => {Age}");
+        Console.WriteLine($"Human thinking => {HumanThinking()}");
+    }
+
+
+}
---
+++ OOPDelegates/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPDelegates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var human = new Human("Omar", 20, () => "Believes in legends");
+        human.DisplayHumanProperties();
+    }
+}
